7eb93d961e6b987f6b8221843686e62d4980f1ee
Josh Mann <josh@jlmann.com>
2021-08-30 07:17:39 -0400

    Initial Commit

---
+++ SalesTax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalesTax
+//
+//  Created by JOSHUA MANN on 8/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
